db9f081dd2a7bacf45b35a42d95f6cdaf4682226
potapovDim <potapovd3012@gmail.com>
2018-02-05 22:37:29 +0200

    base setup service

--- feature/src/main/java/com/example/potapopweblium/initialproject/feature/MainActivity.java
+++ feature/src/main/java/com/example/potapopweblium/initialproject/feature/MainActivity.java
@@ -3,26 +3,45 @@ package com.example.potapopweblium.initialproject.feature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.content.Intent;
+import android.app.Activity;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-    String ms = "android";
+public class MainActivity extends Activity {
+    String ms = "android say :";
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle savedInstanseState) {
+        super.onCreate(savedInstanseState);
         setContentView(R.layout.activity_main);
-        Log.d(ms, "onCreate method called");
-        Log.d(ms, "----------------------");
+        Log.d(ms,"onCreate was runned");
     }
-    @Override
-    protected void onStart(){
-        super.onStart();
-        Log.d(ms, "onStart method called");
-        Log.d(ms, "----------------------");
+
+    public void startService(View view){
+        startService(new Intent(getBaseContext(), HelloService.class));
     }
-    @Override
-    protected void onResume(){
-        super.onResume();
-        Log.d(ms, "onResume method called");
-        Log.d(ms, "----------------------");
+
+    public void stopService(){
+        stopService(new Intent(getBaseContext(), HelloService.class));
     }
+
+    //    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        Log.d(ms, "onCreate method called");
+//        Log.d(ms, "----------------------");
+//    }
+//    @Override
+//    protected void onStart(){
+//        super.onStart();
+//        Log.d(ms, "onStart method called");
+//        Log.d(ms, "----------------------");
+//    }
+//    @Override
+//    protected void onResume(){
+//        super.onResume();
+//        Log.d(ms, "onResume method called");
+//        Log.d(ms, "----------------------");
+//    }
 }
